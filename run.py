388d85251f58147e336551e07423bb40f6e3b9c3
Colm1711 <cosullivan1711@gmail.com>
2022-05-11 18:08:41 +0000

    Add insertion sort function. Add initial Merge sort function structure

--- run.py
+++ run.py
@@ -19,7 +19,8 @@ print(array)
 """
 Description:
 
-This is the bubble sort function. It will take an unorganised array and sort items from smallest to largest. 
+This is the bubble sort function. It will take an unorganised array and sort items from smallest to largest.
+This works by checking adjacent items and swapping items in the list. 
 
 Params:
 
@@ -36,9 +37,10 @@ def bubble_sort(array):
 
         #Loop that will run to sort items. Compares value with agjacent value
         for j in range(array_len - i - 1):
+            #checks to see if first value is greater than adjacent value
             if array[j] > array[j + 1]:
-                #swaps items if adjacent value is greater then item
-                array[j], array[j +1] = array[j + 1], array[j]
+                #swaps items if adjacent value is greater then item value [j]
+                array[j], array[j + 1] = array[j + 1], array[j]
                 array_is_sorted = False
 
         #if there are no swaps or array is sorted then break
@@ -46,6 +48,49 @@ def bubble_sort(array):
             break
     return array
 
-test = bubble_sort(array)
+"""
+Description:
 
-print(test)
+This is the insertion sort function. It will take an unorganised array and sorts the items from smallest to largest.
+Passing items from unsorted list to the sorted list rearranging elements in approproate position.  
+
+Params:
+
+array - one param of the array you would like to sort
+
+"""
+def insertion_sort(array):
+    #get length of array passed into the function
+    array_len = len(array)
+
+    for i in range(1, array_len):
+        
+        #define adjacent element value
+        j = i-1
+
+        nxt_element = array[i]
+      
+        #move elemements of array greater than next element to position of current
+        while j >= 0 and nxt_element < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = nxt_element
+    return array
+
+def merge_sort(array):
+    array_len = len(array)
+    #This will break function if items in list are 1 or less
+    if array_len <= 1:
+        return array
+
+    print("There is sorting to do")
+
+
+# test = bubble_sort(array)
+# print(test)
+
+# test1 = insertion_sort(array)
+# print(test1)
+
+# test2 = merge_sort(array)
+# print(test2)
